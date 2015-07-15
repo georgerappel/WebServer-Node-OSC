2d2ad31660baebe79b176e81d3ce71710022fd94
George Rappel <george.concei@hotmail.com>
2015-07-15 20:00:21 -0300

    Improved some comments

--- tsend.js
+++ tsend.js
@@ -1,9 +1,10 @@
 var osc = require('node-osc');
 var client = new osc.Client('127.0.0.1', 22223);
-// ---- Random function will generate a number between 0 and 1.
 
-//client.send('/lumin/temperatura', 21);
-client.send('/led/posotion', Math.floor((Math.random() * 2)));
+// Random function will generate a number between 0 and 1,
+// So we get the number, multiply by the maximum and use "floor"
+// function to get an integer.
+client.send('/led/position', Math.floor((Math.random() * 2)));
 client.send('/lumin/luminosidade', Math.floor((Math.random() * 100) + 500));
 client.send('/shast/coordenadas', Math.floor((Math.random() * 293)), Math.floor((Math.random() * 293)));
 client.send('/shast/coordenadas', Math.floor((Math.random() * 293)), Math.floor((Math.random() * 293)));
